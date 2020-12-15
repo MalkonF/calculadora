2d4d13198e6d359aa63419ad76f4db3bc85d41e4
Malkon Faria <sudo777@protonmail.com>
2020-12-15 18:39:43 -0300

    Importando modulo calculadora para modulo principal / Configurando barrel para modulo calculadora

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CalculadoraModule } from './calculadora';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, CalculadoraModule],
   providers: [],
   bootstrap: [AppComponent],
 })
