354b8f407162501da6d65dcf54fcc3047e161ca6
Malkon Faria <sudo777@protonmail.com>
2020-12-15 18:27:36 -0300

    first commit

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -7,10 +7,10 @@ const { SpecReporter, StacktraceOption } = require('jasmine-spec-reporter');
 /**
  * @type { import("protractor").Config }
  */
-exports.config = {
+ exports.config = {
   allScriptsTimeout: 11000,
   specs: [
-    './src/**/*.e2e-spec.ts'
+  './src/**/*.e2e-spec.ts'
   ],
   capabilities: {
     browserName: 'chrome'
